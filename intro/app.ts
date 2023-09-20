aeb34f4d23589a2762542427cbd4399b67d43ff5
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-09-21 01:17:25 +0200

    Intro to system messages

--- intro/app.ts
+++ intro/app.ts
@@ -10,6 +10,10 @@ const response = await openai.chat.completions.create({
   temperature: 0,
   messages: [
     {
+      role: "system",
+      content: "You're the BestShoes chat bot. Make sure to introduce yourself as BestShoes Bot on your first interaction with the user."
+    },
+    {
       role: "user",
       content: "Hey, my name is Ariel, greet me.",
     }
