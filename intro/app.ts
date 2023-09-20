1eab46bff93e970499a14fe3484c6f8e4018cb42
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-09-21 00:21:25 +0200

    Run our first chat completion

--- intro/app.ts
+++ intro/app.ts
@@ -1,4 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("Hello World");
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  temperature: 0,
+  messages: [
+    {
+      role: "user",
+      content: "Hey, my name is Ariel, greet me.",
+    }
+  ]
+});
+
+console.log(response.choices);
