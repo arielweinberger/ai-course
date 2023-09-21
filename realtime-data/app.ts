ce766b34be7c8b7d58a28c742f6e55726deece00
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-09-21 22:53:24 +0200

    Start realtime data here

---
+++ realtime-data/app.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import OpenAI from "openai/index.mjs";
+
+const openai = new OpenAI();
+
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  temperature: 0,
+  messages: [
+    {
+      role: "system",
+      content: "You are a helpful assistant that recommends products to users.",
+    },
+    {
+      role: "user",
+      content: "I'm looking for a pair of running shoes.",
+    }
+  ],
+});
+
+console.log(response.choices[0]);
