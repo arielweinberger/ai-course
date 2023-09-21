17faff572ca01414b92cf6d44f6b2de0c97cb400
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-09-21 22:27:24 +0200

    Using the vector store to provide focused context to OpenAI

--- embeddings/app.ts
+++ embeddings/app.ts
@@ -4,12 +4,16 @@ dotenv.config();
 import OpenAI from "openai";
 import fs from "fs";
 import pdf from "@cyber2024/pdf-parse-fixed";
-import { Document, VectorStoreIndex, serviceContextFromDefaults, storageContextFromDefaults } from "llamaindex";
+import { Document, TextNode, VectorStoreIndex, serviceContextFromDefaults, storageContextFromDefaults } from "llamaindex";
 
 const buffer = fs.readFileSync("./thesis.pdf");
 const parsedPdf = await pdf(buffer);
 
-const serviceContext = serviceContextFromDefaults();
+const serviceContext = serviceContextFromDefaults({
+  chunkSize: 4000,
+  chunkOverlap: 500,
+});
+
 const storageContext = await storageContextFromDefaults({
   persistDir: "./storage"
 });
@@ -23,12 +27,33 @@ const index = await VectorStoreIndex.fromDocuments([document], {
 });
 console.log("Index created", index);
 
-// const openai = new OpenAI();
-// const response = await openai.chat.completions.create({
-//   model: "gpt-3.5-turbo",
-//   temperature: 0,
-//   messages: [
-//   ],
-// });
+const query = "Which technologies can be used to solve congestion at airports?";
 
-// console.log(response.choices[0]);
+const retriever = index.asRetriever();
+
+const matchingNodes = await retriever.retrieve(query);
+
+const knowledge = matchingNodes.map(node => {
+  const textNode = node.node as TextNode;
+  return textNode.text;
+}).join("\n\n");
+
+// Querying OpenAI
+
+const openai = new OpenAI();
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  temperature: 0,
+  messages: [
+    {
+      role: "system",
+      content: `You are an aviation expert. Here is your knowledge to answer the user's question: ${knowledge}`
+    },
+    {
+      role: "user",
+      content: query
+    }
+  ],
+});
+
+console.log(response.choices[0]);
