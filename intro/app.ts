98245f6ea99c8d0a8e301286d0f762c47181193f
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-09-21 15:49:11 +0200

    Function calling with examples

--- intro/app.ts
+++ intro/app.ts
@@ -5,10 +5,16 @@ import OpenAI from "openai";
 
 const openai = new OpenAI();
 
-
-
-const goal = "Renovate a kitchen";
-const numTasks = 5;
+const functions: any = {
+  add: (obj: { num1: number, num2: number }) => {
+    const sum = obj.num1 + obj.num2;
+    console.log("Sum is", sum);
+  },
+  subtract: (obj: { num1: number, num2: number }) => {
+    const difference = obj.num1 - obj.num2;
+    console.log("Difference is", difference);
+  }
+}
 
 const response = await openai.chat.completions.create({
   model: "gpt-3.5-turbo",
@@ -16,26 +22,54 @@ const response = await openai.chat.completions.create({
   messages: [
     {
       role: "system",
-      content: `
-        You are a talented task planner.
-        The user will tell you their goal and you will generate a list of tasks for them.
-
-        You must respond in JSON, strinctly following this schema:
-
-        {
-          tasks: {
-            title: string, // Max 100 characters
-            description: string, // Max 120 characters
-            difficulty: "easy" | "medium" | "hard",
-          }[],
-        }
-      `
+      content: "You are a smart calculator."
     },
     {
       role: "user",
-      content: `Tell me how to achieve ${goal}, produce ${numTasks} tasks`,
+      content: "How much is 10 minus 2?"
+    }
+  ],
+  functions: [
+    {
+      name: "add",
+      description: "Adds two numbers",
+      parameters: {
+        type: "object",
+        properties:{
+          num1: {
+            type: "number",
+          },
+          num2: {
+            type: "number",
+          },
+        }
+      }
+    },
+    {
+      name: "subtract",
+      description: "Subtracts two numbers",
+      parameters: {
+        type: "object",
+        properties:{
+          num1: {
+            type: "number",
+          },
+          num2: {
+            type: "number",
+          },
+        }
+      }
     }
   ]
 });
 
-console.log(JSON.parse(response.choices[0].message.content as string));
+console.log(response.choices[0]);
+const function_call = response.choices[0].message.function_call;
+
+if (function_call) {
+  const name = function_call.name;
+  const args = JSON.parse(function_call.arguments);
+  const fn = functions[name];
+  fn(args);
+}
+ 
